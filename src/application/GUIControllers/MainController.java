778afccaf2abed7c77341e10a8016ee4208dc207
Ben Hayward <benhayward93@hotmail.com>
2017-04-05 01:15:29 +0100

    Set up the onClick and used tabpane instead of the card layout. A small amount of organization too. Next I need to develop the other panes, then connect everything through their respective onClick() methods.

--- src/application/GUIControllers/MainController.java
+++ src/application/GUIControllers/MainController.java
@@ -2,17 +2,41 @@ package application.GUIControllers;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javafx.scene.control.Button;
+
 /**
  * Controller of the main screen of the GUI.
  * @author Ben Hayward
  */
 public class MainController implements MouseListener{
 	
+	private Button findPatientButton, addAppointmentsButton, findAppointmentsButton, 
+	addPatientButton, exportResultsButton, notesButton, diagramButton;
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
+		if (e.getSource() == findPatientButton){
+		
+		}
+		if (e.getSource() == addAppointmentsButton){
+			
+		}
+		if (e.getSource() ==  findAppointmentsButton){
+			
+		}
+		if (e.getSource() ==  addPatientButton){
+				
+		}			
+		if (e.getSource() ==  exportResultsButton){
+				
+		} 			
+		if (e.getSource() ==  notesButton){
+				
+		}	
+		if (e.getSource() ==  diagramButton){
+				
+		}			
 	}
 
 	@Override
